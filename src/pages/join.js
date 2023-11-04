3315d421f570db1e790e9cdcde91b5e8fed68db0
smarthug <kirklayer@gmail.com>
2023-11-04 19:19:58 +0900

    wip: add user info

--- src/pages/join.js
+++ src/pages/join.js
@@ -1,107 +1,127 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import { auth } from "../utils/firebase";
+import { auth,db } from "../utils/firebase";
 // import { Link, useNavigate } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
 import {
-  Form,
-  Error,
-  Input,
-  Switcher,
-  Title,
-  Wrapper,
+    Form,
+    Error,
+    Input,
+    Switcher,
+    Title,
+    Wrapper,
 } from "../components/auth-components";
 import GithubButton from "../components/github-btn";
 
 import { createGlobalStyle, styled } from "styled-components";
 
+import { useRouter } from 'next/router'
+import Link from "next/link";
+import { Button } from "@mui/material";
+
+
 export default function CreateAccount() {
-//   const navigate = useNavigate();
-  const [isLoading, setLoading] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const onChange = (e) => {
-    const {
-      target: { name, value },
-    } = e;
-    if (name === "name") {
-      setName(value);
-    } else if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
+    const router = useRouter()
+    //   const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const onChange = (e) => {
+        const {
+            target: { name, value },
+        } = e;
+        if (name === "name") {
+            setName(value);
+        } else if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        if (isLoading || name === "" || email === "" || password === "") return;
+        try {
+            setLoading(true);
+            const credentials = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            await updateProfile(credentials.user, {
+                displayName: name,
+            });
+            router.push('/')
+        } catch (e) {
+            if (e instanceof FirebaseError) {
+                setError(e.message);
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    async function AddUserInfo(){
+
+        const user = auth.currentUser;
+
+        const doc = await addDoc(collection(db, "users"), {
+            tweet,
+            createdAt: Date.now(),
+            username: user.displayName || "Anonymous",
+            userId: user.uid,
+          });
     }
-  };
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    if (isLoading || name === "" || email === "" || password === "") return;
-    try {
-      setLoading(true);
-      const credentials = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      await updateProfile(credentials.user, {
-        displayName: name,
-      });
-    //   navigate("/");
-    } catch (e) {
-      if (e instanceof FirebaseError) {
-        setError(e.message);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-  return (
-    <BigWrapper>
+    return (
+        <BigWrapper>
 
 
-    <Wrapper>
-      <Title>Join MinterLab</Title>
-      <Form onSubmit={onSubmit}>
-        <Input
-          onChange={onChange}
-          name="name"
-          value={name}
-          placeholder="Name"
-          type="text"
-          required
-        />
-        <Input
-          onChange={onChange}
-          name="email"
-          value={email}
-          placeholder="Email"
-          type="email"
-          required
-        />
-        <Input
-          onChange={onChange}
-          value={password}
-          name="password"
-          placeholder="Password"
-          type="password"
-          required
-        />
-        <Input
-          type="submit"
-          value={isLoading ? "Loading..." : "Create Account"}
-        />
-      </Form>
-      {error !== "" ? <Error>{error}</Error> : null}
-      <Switcher>
-        {/* Already have an account? <Link to="/login">Log in &rarr;</Link> */}
-      </Switcher>
-      <GithubButton />
-    </Wrapper>
+            <Wrapper>
+                <Title>Join MinterLab</Title>
+                <Form onSubmit={onSubmit}>
+                    <Input
+                        onChange={onChange}
+                        name="name"
+                        value={name}
+                        placeholder="Name"
+                        type="text"
+                        required
+                    />
+                    <Input
+                        onChange={onChange}
+                        name="email"
+                        value={email}
+                        placeholder="Email"
+                        type="email"
+                        required
+                    />
+                    <Input
+                        onChange={onChange}
+                        value={password}
+                        name="password"
+                        placeholder="Password"
+                        type="password"
+                        required
+                    />
+                    <Input
+                        type="submit"
+                        value={isLoading ? "Loading..." : "Create Account"}
+                    />
+                </Form>
+                {error !== "" ? <Error>{error}</Error> : null}
+                <Switcher>
+                    Already have an account? <Link href="/login">Log in &rarr;</Link>
+                </Switcher>
+                <GithubButton />
 
-    </BigWrapper>
-  );
+                <Button variant="contained" color="primary" onClick={AddUserInfo} style={{ marginTop: "20px" }}></Button>
+            </Wrapper>
+
+        </BigWrapper>
+    );
 }
 
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -4,77 +4,93 @@ import { auth } from "../utils/firebase";
 import { FirebaseError } from "firebase/app";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import {
-  Error,
-  Form,
-  Input,
-  Switcher,
-  Title,
-  Wrapper,
+    Error,
+    Form,
+    Input,
+    Switcher,
+    Title,
+    Wrapper,
 } from "../components/auth-components";
 import GithubButton from "../components/github-btn";
-import GoogleButton from "../components/google-btn";
+// import GoogleButton from "../components/google-btn";
+
+import { useRouter } from 'next/router'
+import Link from "next/link";
+import styled from "styled-components";
+import { Button } from "@mui/material";
 
 export default function CreateAccount() {
-//   const navigate = useNavigate();
-  const [isLoading, setLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const onChange = (e) => {
-    const {
-      target: { name, value },
-    } = e;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-  };
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    if (isLoading || email === "" || password === "") return;
-    try {
-      setLoading(true);
-      await signInWithEmailAndPassword(auth, email, password);
-      navigate("/");
-    } catch (e) {
-      if (e instanceof FirebaseError) {
-        setError(e.message);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-  return (
-    <Wrapper>
-      <Title>Log into 𝕏</Title>
-      <Form onSubmit={onSubmit}>
-        <Input
-          onChange={onChange}
-          name="email"
-          value={email}
-          placeholder="Email"
-          type="email"
-          required
-        />
-        <Input
-          onChange={onChange}
-          value={password}
-          name="password"
-          placeholder="Password"
-          type="password"
-          required
-        />
-        <Input type="submit" value={isLoading ? "Loading..." : "Log in"} />
-      </Form>
-      {error !== "" ? <Error>{error}</Error> : null}
-      <Switcher>
-        Don't have an account?{" "}
-        {/* <Link to="/create-account">Create one &rarr;</Link> */}
-      </Switcher>
-      <GithubButton />
-      <GoogleButton />
-    </Wrapper>
-  );
+
+    const router = useRouter()
+    const [isLoading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const onChange = (e) => {
+        const {
+            target: { name, value },
+        } = e;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        if (isLoading || email === "" || password === "") return;
+        try {
+            setLoading(true);
+            await signInWithEmailAndPassword(auth, email, password);
+            router.push("/");
+        } catch (e) {
+            if (e instanceof FirebaseError) {
+                setError(e.message);
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+    return (
+        <BigWrapper>
+            <Wrapper>
+                <Title>Log into MinterLab</Title>
+                <Form onSubmit={onSubmit}>
+                    <Input
+                        onChange={onChange}
+                        name="email"
+                        value={email}
+                        placeholder="Email"
+                        type="email"
+                        required
+                    />
+                    <Input
+                        onChange={onChange}
+                        value={password}
+                        name="password"
+                        placeholder="Password"
+                        type="password"
+                        required
+                    />
+                    <Input type="submit" value={isLoading ? "Loading..." : "Log in"} />
+                </Form>
+                {error !== "" ? <Error>{error}</Error> : null}
+                <Switcher>
+                    Don't have an account?{" "}
+                    <Link href="/join">Create one &rarr;</Link>
+                </Switcher>
+                <GithubButton />
+                
+                {/* <GoogleButton /> */}
+            </Wrapper>
+        </BigWrapper>
+    );
 }
+
+
+const BigWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
