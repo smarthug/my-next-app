32713ed6708ad8de9e092e44554484e46f298dcb
smarthug <kirklayer@gmail.com>
2023-11-08 17:41:43 +0900

    row made

--- src/pages/creators/create.js
+++ src/pages/creators/create.js
@@ -19,6 +19,8 @@ import { useState } from 'react';
 
 import { Card, CardContent, Avatar, Typography, Button } from '@mui/material';
 
+import ProjectInputRow from '../../components/ProjectInputRow';
+
 
 
 export default function ColumnsGrid() {
@@ -32,8 +34,11 @@ export default function ColumnsGrid() {
 
 
     return (
-        <Box sx={{ width: '100%' }}>
-            <h1>프로젝트 기획</h1>
+        <Box sx={{ width: '100%', marginTop: "64px" }}>
+            <Typography variant="h4" component="div" gutterBottom>
+                프로젝트 기획
+            </Typography>
+
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
                     <Tab label="기본정보" />
@@ -46,7 +51,16 @@ export default function ColumnsGrid() {
 
 
 
-                기본정보
+                <ProjectInputRow label="프로젝트 카테고리" description="프로젝트 성격과 가장 일치하는 카테고리를 선택해주세요.
+적합하지 않을 경우 운영자에 의해 조정될 수 있습니다.">
+
+                    <input type="text" placeholder="프로젝트 이름을 입력하세요" />
+                </ProjectInputRow>
+
+                <ProjectInputRow label="프로젝트 제목" description="프로젝트의 주제, 창작물의 특징이 드러나는 멋진 제목을 붙여주세요.">
+
+                    <input type="text" placeholder="프로젝트 이름을 입력하세요" />
+                </ProjectInputRow>
 
             </CustomTabPanel>
             <CustomTabPanel value={value} index={1}>
