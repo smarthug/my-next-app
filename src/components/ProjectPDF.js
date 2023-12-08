a466de38883607577f3ed43e66be9217e1fe228b
smarthug <kirklayer@gmail.com>
2023-12-08 17:18:21 +0900

    pdf test done

---
+++ src/components/ProjectPDF.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+// import {  } from 'react-pdf';
+
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   'pdfjs-dist/build/pdf.worker.min.js',
+//   import.meta.url,
+// ).toString();
+
+// import { pdfjs } from 'react-pdf';
+
+// pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+export default function MyApp() {
+    const [numPages, setNumPages] = useState();
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
+
+    return (
+        <div>
+            <Document file="/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} />
+            </Document>
+            <p>
+                Page {pageNumber} of {numPages}
+            </p>
+        </div>
+    );
+}
---
+++ src/pages/project/pdf.js
@@ -0,0 +1,22 @@
+import dynamic from "next/dynamic"
+import { useRouter } from "next/router";
+
+
+
+const ProjectPDF = dynamic(() => import('@/components/ProjectPDF'), {
+    ssr: false
+})
+
+export default function Project() {
+
+    const {
+        query: { projectId }
+    } = useRouter();
+
+
+    return (
+        <div>
+            <ProjectPDF projectId={projectId} />
+        </div>
+    )
+}   
